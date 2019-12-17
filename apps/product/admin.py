fb79030c3faf795defb06769eddade8eee87cd38
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2019-12-16 20:27:02 -0500

    validate unique name category product in the side admin

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,10 +1,21 @@
-from django.contrib.contenttypes.admin import GenericTabularInline
+from django.db import transaction
+from django.contrib import messages
 from django.contrib import admin
 from .models import Product, Category
 
 @admin.register(Category)
-class ProductAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
+    def save_model(self, request, obj, form, change):
+            with transaction.atomic():
+                try:
+                    super().save_model(request, obj, form, change)
+                except Exception as e:
+                    messages.set_level(request, messages.ERROR)
+                    messages.error(request, e)
+
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
--- carniceria/urls.py
+++ carniceria/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-
-    urlpatterns += static(settings.MEDIA_URL,
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns + static(settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT)
